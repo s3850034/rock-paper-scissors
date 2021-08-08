737739c332be08062f38a38a261f49984640e88f
Muhib <s3850034@student.rmit.edu.au>
2021-08-08 23:57:55 +1000

    Finish Game Functionality

--- script.js
+++ script.js
@@ -14,3 +14,51 @@ function computerPlay() {
     }
 
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+    let didYouWin = (playerSelection === "Scissors" && computerSelection === "Paper") ||
+                    (playerSelection === "Paper" && computerSelection === "Rock") || 
+                    (playerSelection === "Rock" && computerSelection === "Scissors");
+    
+    return didYouWin;
+}
+
+function capitalize(value) {
+    let rest = value.substring(1, value.length).toLowerCase();
+    let start = value.charAt(0).toUpperCase();
+    return start + rest;
+}
+
+function isDraw(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+    let draw =  (playerSelection === "Scissors" && computerSelection === "Scissors") ||
+                  (playerSelection === "Paper" && computerSelection === "Paper") || 
+                  (playerSelection === "Rock" && computerSelection === "Rock");
+
+    return draw;
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, Scissors?");
+        let computerSelection = computerPlay();
+
+        if (playRound(playerSelection, computerSelection)) {
+            console.log("You Win! " + capitalize(playerSelection) + " beats " + computerSelection);
+        } else if (isDraw(playerSelection, computerSelection)) {
+            console.log("You Drew! You picked the same option as the computer");
+        } else {
+            console.log("You Lose! " +  computerSelection + " beats " + capitalize(playerSelection));
+        }
+    }
+}
+
+game();
+
+
+
